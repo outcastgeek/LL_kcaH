fd154a4ce0c894250dbee1af3cc0f31b1c005393
bebby <benjamin_ebby@optum.com>
2017-11-29 18:46:09 -0600

    Updated Build + Setup

--- kcah2ing/firstprog/firstprog.c
+++ kcah2ing/firstprog/firstprog.c
@@ -5,7 +5,7 @@ int main() {
   int i;
   for (i = 0; i < 10; i++)  // Loop 10 times.
   {
-    puts("Hello, world!\n");  // put the string to the output.
+    puts("Hello, world :-) !\n");  // put the string to the output.
   }
   return 0;  // Tell OS the program exited without errors.
 }
