919dde349672c00120229cbeb9c5cb962421ebeb
outcastgeek <outcastgeek+git@gmail.com>
2018-06-24 21:18:36 -0500

    WIP + Tidy Up of CMakefile.txt

---
+++ kcah2ing/typecasting/.dir-locals.el
@@ -0,0 +1,20 @@
+;; https://projectile.readthedocs.io/en/latest/configuration/
+;; Projectile Configuration
+((nil . ((eval . (setq projectile-project-root
+                       (locate-dominating-file buffer-file-name
+                                               ".dir-locals.el")))
+         (eval . (setq compile-command
+                       `(format "cd %s && make"
+                                (locate-dominating-file buffer-file-name
+                                                        ".dir-locals.el"))))
+         ;; In C code indentation is 4 spaces and in C++ 2 spaces
+         (c++-mode
+          (indent-tabs-mode . nil)
+          (c-basic-offset . 2))
+         (c-mode
+          (indent-tabs-mode . nil)
+          (c-basic-offset . 4)
+          (c-file-style . "Google")
+          (whitespace-style face lines indentation:space))
+         )))
+
