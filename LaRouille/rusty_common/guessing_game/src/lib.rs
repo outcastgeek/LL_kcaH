5c477ade0dbce10b499a7b249310a43b301eb3ad
outcastgeek <outcastgeek+git@gmail.com>
2018-07-25 17:39:46 -0500

    Rust WIP

--- LaRouille/rusty_common/guessing_game/src/lib.rs
+++ LaRouille/rusty_common/guessing_game/src/lib.rs
@@ -1,17 +1,36 @@
+
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 #[no_mangle]
 pub extern fn run() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng()
+        .gen_range(1, 101);
+
+    print!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess  = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
 #[cfg(test)]
