9ab8af7ecb77a6507442eb3577d71c83c4f46320
outcastgeek <outcastgeek+git@gmail.com>
2019-01-31 13:31:14 -0600

    Hacker Rank -- Hello and InOut

---
+++ HackerRank/HR_InputOutput/hr_input_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+    std::cin >> a >> b >> c;
+
+    int res = a + b + c;
+    std::cout << res << std::endl;
+
+    return 0;
+}
